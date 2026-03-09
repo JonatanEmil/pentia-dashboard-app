b4889287276aecf0c48add24cea2fe330c7cbddc
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2026-03-09 13:31:06 +0100

    ESLint opsætning og testing (#2)

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,24 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+
+interface T { x: number }
+const john: string = 'Cena';
 
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+    const count = ref(0);
+    const doubleCount = computed(() => count.value * 2);
 
-  return { count, doubleCount, increment }
-})
+    function increment(): void {
+        count.value++;
+    }
+    const array: number[] = [
+        1,
+        2,
+        3,
+    ];
+    const y: string[] = ['a', 'b'];
+
+  interface RecordType { [key: string]: unknown }
+
+  return { count, doubleCount, increment };
+});
