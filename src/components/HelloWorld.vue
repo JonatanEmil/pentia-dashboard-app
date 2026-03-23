bf7c5ab3d7d2ce488b83cef7008ba0bb25f5c9c1
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-03-23 10:56:17 +0100

    Add nodemon in setup process

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
 defineProps<{
   msg: string
 }>()
+console.log("hej")
 </script>
 
 <template>
@@ -10,6 +11,7 @@ defineProps<{
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+    
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div>
