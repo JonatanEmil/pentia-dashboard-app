9a5c573aea68b8ed4446c481bbfa45c25159bdd3
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-03-23 12:33:38 +0100

    Add svg to assets

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -11,6 +11,8 @@ import WelcomeItem from './WelcomeItem.vue';
     </template>
     <template #heading>Documentation</template>
 
+
+
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
     provides you with all information you need to get started.
