97b8743e5274ce67b7737a3474fc7437b47dcdaa
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-03-30 12:39:11 +0200

    Create login form vue component and scss #5 #6

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -7,7 +7,7 @@ import InputText from '@/components/InputText.vue';
 </script>
 
 <template>
-    <form>
+    <form class="flex flex--just-center flex--column gap--1">
 
     <InputText placeholderText="Brugernavn"/>
     <InputText placeholderText="Password"/>
