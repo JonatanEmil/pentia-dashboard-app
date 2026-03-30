07aeaeaf379473713a1654b2b3e4ecfdafef7537
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-03-30 13:29:42 +0200

    Add global dimensions file for styling #1

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -9,8 +9,8 @@ import InputText from '@/components/InputText.vue';
 <template>
     <form class="flex flex--column flex--align-center flex--gaprow-4">
 
-    <InputText placeholderText="Brugernavn" class="p--4"/>
-    <InputText placeholderText="Password"/>
+    <InputText placeholderText="Brugernavn" class="py--3"/>
+    <InputText placeholderText="Password" class="py--3"/>
 
     <FormButtonSubmit class="formButtonSubmit">Login</FormButtonSubmit>
     </form>
