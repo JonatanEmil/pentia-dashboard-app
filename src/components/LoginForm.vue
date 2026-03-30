7cfaebf1916c02698f8b4de057dd32df7349a583
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-03-30 10:58:48 +0200

    Add login form with template #8

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import FormButtonSubmit from '@/components/FormButtonSubmit.vue';
+import InputText from '@/components/InputText.vue';
+
+
+</script>
+
+<template>
+    <form>
+
+    <InputText placeholderText="Brugernavn"/>
+    <InputText placeholderText="Password"/>
+
+    <FormButtonSubmit class="formButtonSubmit">Login</FormButtonSubmit>
+    </form>
+</template>
--- src/views/NannaTestView.vue
+++ src/views/NannaTestView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import FormButtonSubmit from '@/components/FormButtonSubmit.vue';
 import '@/assets/scss/main.scss';
+import LoginForm from '@/components/LoginForm.vue';
 
 </script>
 
@@ -8,6 +9,8 @@ import '@/assets/scss/main.scss';
     <FormButtonSubmit class="formButtonSubmit">Opret</FormButtonSubmit>
 
     <FormButtonSubmit class="formButtonSubmit">Login</FormButtonSubmit>
+
+    <LoginForm></LoginForm>
 
 </template>
 
