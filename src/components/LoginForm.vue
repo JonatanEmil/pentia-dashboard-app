7f52c834dc5f832d656a15681f2970aea513f4bb
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-03-30 13:10:06 +0200

    SCSS for login form #6

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -7,9 +7,9 @@ import InputText from '@/components/InputText.vue';
 </script>
 
 <template>
-    <form class="flex flex--just-center flex--column gap--1">
+    <form class="flex flex--column flex--align-center flex--gaprow-4">
 
-    <InputText placeholderText="Brugernavn"/>
+    <InputText placeholderText="Brugernavn" class="p--4"/>
     <InputText placeholderText="Password"/>
 
     <FormButtonSubmit class="formButtonSubmit">Login</FormButtonSubmit>
