519f8af8f457f2a8d8e4b6e38648543bb209f22f
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-03-30 10:18:55 +0200

    Add scss for formButtonSubmit - used in test area #14

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
             // which is lazy-loaded when the route is visited.
             component: () => import('../views/AboutView.vue'),
         },
+        {
+            path: '/Nannatestarea',
+            name: 'Nannatestarea',
+            component: () => import('../views/NannaTestView.vue'),
+        }
     ],
 })
 
--- src/views/NannaTestView.vue
+++ src/views/NannaTestView.vue
@@ -1,26 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
-
+import FormButtonSubmit from '@/components/FormButtonSubmit.vue';
+import '@/assets/scss/main.scss';
 
 </script>
 
 <template>
-  <header>
+    <FormButtonSubmit class="formButtonSubmit">Opret</FormButtonSubmit>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!"/>
-        <p class="font--h1">Fundament</p>
+    <FormButtonSubmit class="formButtonSubmit">Login</FormButtonSubmit>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/Nannatestarea">Nanna Test Area</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
