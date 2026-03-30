a13af26eefda3f3c6d64af3befa7f8eaf02b6ab4
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2026-03-30 12:15:28 +0200

    add GeneralCard.vue template #92

---
+++ src/components/GeneralCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+
+
+</script>
+<template>
+    <div class="card">
+        <div class="card-img">
+            <img :src="imageSource" :alt="'Et billede af ' + picture.name">
+        </div>
+        <div class="card-content">
+            {{textContent}}
+        </div>
+    </div>
+</template>
