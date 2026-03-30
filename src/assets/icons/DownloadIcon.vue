7c2403a624936010ac660183bf42ee49ff8440e8
Valdemar <pizza27@live.dk>
2026-03-30 13:29:37 +0200

    Opdateret Ikoner til vue filer
    Så de understøtter fill css

---
+++ src/assets/icons/DownloadIcon.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="92" height="92" viewBox="0 0 92 92" xmlns="http://www.w3.org/2000/svg">
+        <path d="M46 1H61V37H79.75L46 64M46 1H31V37H12.25L46 64" fill="currentColor" stroke="currentColor" stroke-width="2"
+            stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M1 91H91" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+</template>
---
+++ src/assets/icons/MicrophoneIcon.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg width="98" height="98" viewBox="0 0 98 98" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+        <rect x="4" width="90" height="90" fill="currentColor" shape-rendering="crispEdges" />
+    </svg>
+</template>
