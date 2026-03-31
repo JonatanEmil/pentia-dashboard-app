0a6f57e4860cfacff724c42adc61f42b3d49437e
oliven1212 <60175607+oliven1212@users.noreply.github.com>
2026-03-31 15:22:40 +0200

    Created ItemList component #30 #76 #78 #80

---
+++ src/views/ValdemarTestView.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import ItemList from '@/components/ItemList.vue';
+import NavbarButton from '@/components/NavbarButton.vue';
+import { defineAsyncComponent } from 'vue';
+
+const testList = [
+    
+    {
+        name: 'Tilføj',
+        routeName: 'createClient',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/AddIcon.vue')),
+    },
+    {
+        name: 'Hjem',
+        routeName: 'home',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/HomeIcon.vue')),
+    },
+    {
+        name: 'Tilføj',
+        routeName: 'createClient',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/AddIcon.vue')),
+    },
+    {
+        name: 'Hjem',
+        routeName: 'home',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/HomeIcon.vue')),
+    },
+    {
+        name: 'Tilføj',
+        routeName: 'createClient',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/AddIcon.vue')),
+    },
+    {
+        name: 'Hjem',
+        routeName: 'home',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/HomeIcon.vue')),
+    },
+    {
+        name: 'Tilføj',
+        routeName: 'createClient',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/AddIcon.vue')),
+    },
+    {
+        name: 'Hjem',
+        routeName: 'home',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/HomeIcon.vue')),
+    },
+    {
+        name: 'Tilføj',
+        routeName: 'createClient',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/AddIcon.vue')),
+    },
+    {
+        name: 'Hjem',
+        routeName: 'home',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/HomeIcon.vue')),
+    },
+    {
+        name: 'Tilføj',
+        routeName: 'createClient',
+        svgComponent: defineAsyncComponent(() => import('@/assets/icons/AddIcon.vue')),
+    },
+];
+</script>
+
+<template>
+    <ItemList :items="testList" v-slot="{ item }" :columns="5" :titel="'fancy titel'">
+        <NavbarButton 
+        :name="item.name" 
+        :route-name="item.routeName" 
+        :svg-component="item.svgComponent" 
+        />
+    </ItemList>
+</template>
+
