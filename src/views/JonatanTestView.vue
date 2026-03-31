50506e016f74e76b38817920eb51ba49d3c3652b
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2026-03-31 14:09:27 +0200

    add RadioButton.vue logic #153

--- src/views/JonatanTestView.vue
+++ src/views/JonatanTestView.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import '@/assets/scss/main.scss';
-import '@/components/RadioButton.vue';
+import RadioButton from '@/components/RadioButton.vue';
+import { ref } from 'vue';
 
+const selected = ref(null);
+const options = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
 
 </script>
 
 <template>
-
-
+    <RadioButton
+        v-for="option in options"
+        :key="option"
+        v-model="selected"
+        :value="option"
+    >
+        {{ option }}
+    </RadioButton>
 </template>
