4cc8e3f84688ae2f88bd2567d53f232c78c53b39
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-04-10 13:31:13 +0200

    Add TS and speaktotext funktion #175

--- src/components/SpeakToText.vue
+++ src/components/SpeakToText.vue
@@ -14,6 +14,45 @@ const listening = ref(false);
 const error = ref<string | null>(null);
 const listener = ref<InstanceType<typeof SpeechToText> | null>(null);
 
+watch(
+    [listening, listener],
+    (_, __, onCleanup) => {
+        const onAnythingSaid = (text: string) => {
+            interimText.value = '';
+            emit('interimText', text);
+        };
+
+        const onEndEvent = () => {
+            if (listening.value) {
+                listner.value?.startListening();
+            }
+        };
+
+        const onFinalised = (text: string) => {
+            finalistedText.value = [...finalisedText.value];
+            interimText.value = '';
+            emit('update:modelValue', text);
+        };
+
+        try {
+            listner.value = new SpeechToText(onAnythingSaid, onEndEvent, onFinalised);
+            error.value = null;
+        } catch (err) {
+            if (err instanceof Error) {
+                error.value = err.message;
+            }
+        }
+
+        onCleanup(() => {
+            listener.value?.stopListening();
+        });
+
+    },
+
+    { immediate: true },
+
+);
+
 </script>
 
 <template>
@@ -21,5 +60,5 @@ const listener = ref<InstanceType<typeof SpeechToText> | null>(null);
     <div>
 
     </div>
-    
+
 </template>
