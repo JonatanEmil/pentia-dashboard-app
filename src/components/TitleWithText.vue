c52d1258e83f47ef96a738b00c8d8b70e8984723
oliven1212 <60175607+oliven1212@users.noreply.github.com>
2026-04-10 08:41:38 +0200

    TitleWithText.vue fuldt opretted #36 #129 #130 #131

---
+++ src/components/TitleWithText.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+defineProps<{
+  title: string,
+  text: string,
+}>();
+
+
+</script>
+<template>
+    <div>
+        <h1 class="font--h1" v-if="title" >{{ title }}</h1>
+        <p class="font--p" v-if="text" >{{ text }}</p>
+    </div>
+</template>
--- src/views/ValdemarTestView.vue
+++ src/views/ValdemarTestView.vue
@@ -2,7 +2,7 @@
 import ItemList from '@/components/ItemList.vue';
 import NavbarButton from '@/components/NavbarButton.vue';
 import { defineAsyncComponent } from 'vue';
-
+import TitleWithText from '@/components/TitleWithText.vue';
 const testList = [
     
     {
@@ -64,6 +64,10 @@ const testList = [
 </script>
 
 <template>
+    <TitleWithText
+        title="stevo" 
+        text="hello world this is the start of a long text"
+    />
     <ItemList :items="testList" v-slot="{ item }" :columns="5" :titel="'fancy titel'">
         <NavbarButton 
         :name="item.name" 
