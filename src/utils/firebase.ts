5ba5840cc0b13f7a3860f90018dd050e3685d428
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-04-14 13:14:11 +0200

    Init data and firestore

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDoX8RERHYBYsvvBrkXLsBTVEI6eQVVW54',
-    authDomain: 'pentia-dashboard-app.firebaseapp.com',
-    projectId: 'pentia-dashboard-app',
-    storageBucket: 'pentia-dashboard-app.firebasestorage.app',
-    messagingSenderId: '178462860487',
-    appId: '1:178462860487:web:70a5bc1952dc17edf60ae2',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
 };
 
 const app = initializeApp(firebaseConfig);
