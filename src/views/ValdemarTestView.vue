d7ef27dfc89c9fb51b52713b31f201b1bfbbfa1d
oliven1212 <60175607+oliven1212@users.noreply.github.com>
2026-04-20 09:43:01 +0200

    InfoCard oprettet #57 #194 #196 #197

--- src/views/ValdemarTestView.vue
+++ src/views/ValdemarTestView.vue
@@ -4,6 +4,7 @@ import NavbarButton from '@/components/NavbarButton.vue';
 import { defineAsyncComponent } from 'vue';
 import TitleWithText from '@/components/TitleWithText.vue';
 import ProgressCard from '@/components/ProgressCard.vue';
+import InfoCard from '@/components/InfoCard.vue';
 const testList = [
     
     {
@@ -65,15 +66,22 @@ const testList = [
 </script>
 
 <template>
+    <InfoCard 
+    :text="'a extremely fancy body text with loads of text that automatically wraps to the next li'"
+    :title="'a very fancy title with lots of text'"
+    />
+
+
     <ProgressCard text="a fancy link" routeName="home" status="done"/>
     <br>
     <ProgressCard text="a fancy link" routeName="home" status="planned"/>
+
 
     <TitleWithText
         title="stevo" 
         text="hello world this is the start of a long text"
     />
-    <ItemList :items="testList" v-slot="{ item }" :columns="5" :titel="'fancy titel'">
+    <ItemList :items="testList" v-slot="{ item }" :columns="5" :title="'fancy title'">
         <NavbarButton 
         :name="item.name" 
         :route-name="item.routeName" 
