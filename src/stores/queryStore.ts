4298b96def322d7b283f206d0bfd2b3e9dd8c55a
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2026-04-27 11:36:31 +0200

    init queryStore.ts #230

---
+++ src/stores/queryStore.ts
@@ -0,0 +1,34 @@
+import { defineStore } from 'pinia';
+import { ref, computed } from 'vue';
+import { db } from '@/utils/firebase.ts';
+import { collection, getDocs } from 'firebase/firestore';
+import { useBookingStore } from '@/stores/bookingStore.ts';
+import { useBuildingStepStore } from '@/stores/buildingStep.ts';
+import { useCaseStore } from '@/stores/caseStore.ts';
+import { useFileStore } from '@/stores/fileStore.ts';
+import { useImageStore } from '@/stores/imageStore.ts';
+import { useMessageStore } from '@/stores/messageStore.ts';
+import { useUserStore } from "@/stores/userStore.ts";
+
+/*const bookingStore = useBookingStore();
+const buildingStepStore = useBuildingStepStore();
+const caseStore = useCaseStore();
+const fileStore = useFileStore();
+const imageStore = useImageStore();
+const messageStore = useMessageStore();
+const userStore = useUserStore();*/
+
+
+export const useQueryStore = defineStore('query', () => {
+    // State
+
+
+    // Getters
+
+
+    // Actions
+
+    return {
+
+    };
+});
