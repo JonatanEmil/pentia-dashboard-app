03c39de802048039a3c0d500cada401fa1f35dbb
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-04-30 14:34:17 +0200

    Update userstore

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,9 +1,11 @@
 import { defineStore } from 'pinia';
-import { ref, computed } from 'vue';
+import { ref } from 'vue';
 import { db } from '@/config/firebase';
-import { collection, getDocs, query, where, doc, type DocumentReference } from 'firebase/firestore';
+import { collection, getDocs, query, where, doc, getDoc, type DocumentReference } from 'firebase/firestore';
+import { useAuthStore } from './authStore.ts';
 
 export interface User {
+    id: string
     email: string
     firstName: string
     lastName: string
@@ -11,16 +13,14 @@ export interface User {
     phoneNumber: string
     role: string
     caseId: DocumentReference[]
+    imageId: DocumentReference
 }
-
 
 export const useUserStore = defineStore('user', () => {
     // State
     const userList = ref<User[]>([]);
-    const currentUser = ref<User>();
-
-
-    // Getters
+    const clientList = ref<User[]>([]);
+    const currentUser = ref<User | null>(null);
 
     // Actions
     async function getUserList(): Promise<void> {
@@ -29,14 +29,13 @@ export const useUserStore = defineStore('user', () => {
         snapshot.forEach((doc) => {
             userList.value.push(doc.data() as User);
         });
-
     }
 
     async function getManagerForCase(caseId: string): Promise<string> {
         const q = query(
             collection(db, 'users'),
             where('role', '==', 'manager'),
-            where('caseId', 'array-contains', doc(db, 'cases', caseId))
+            where('caseId', 'array-contains', doc(db, 'cases', caseId)),
         );
 
         const snapshot = await getDocs(q);
@@ -44,12 +43,39 @@ export const useUserStore = defineStore('user', () => {
         return snapshot.docs[0]?.id ?? '';
     }
 
-    // Returns the state, getters and actions
+    async function getClientsForManager(): Promise<void> {
+        const authStore = useAuthStore();
+
+        const managerDoc = await getDoc(doc(db, 'users', authStore.currentUser!.id));
+        const caseIds = managerDoc.data()?.caseId as DocumentReference[];
+
+        clientList.value = [];
+
+        for (const caseRef of caseIds) {
+            const q = query(
+                collection(db, 'users'),
+                where('role', '==', 'client'),
+                where('caseId', 'array-contains', caseRef),
+            );
+
+            const snapshot = await getDocs(q);
+
+            snapshot.docs.forEach((document) => {
+                clientList.value.push({
+                    
+                    ...document.data() as User,
+                    id: document.id,
+                });
+            });
+        }
+    }
+
     return {
         userList,
+        clientList,
         currentUser,
         getUserList,
         getManagerForCase,
+        getClientsForManager,  
     };
 });
-
