14d48ade69523d5b902e28ed5fafc94ba2efb02e
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2026-04-30 15:16:54 +0200

    add homeview template #88

--- src/views/manager/HomeView.vue
+++ src/views/manager/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+import SearchBar from '@/components/SearchBar.vue';
 </script>
 
 <template>
   <main>
-
+      <SearchBar class="flex--just-self-end"></SearchBar>
   </main>
 </template>
