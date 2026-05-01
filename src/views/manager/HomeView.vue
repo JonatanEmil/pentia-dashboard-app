19f4b5d5d350c04e3e15bfb56b823cd7dda98fca
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2026-05-01 10:22:52 +0200

    add for image compatability manager/HomeView.vue #88

--- src/views/manager/HomeView.vue
+++ src/views/manager/HomeView.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
 import GeneralCard from '@/components/GeneralCard.vue';
 import ItemList from '@/components/ItemList.vue';
+import ProfilePictureWithName from "@/components/ProfilePictureWithName.vue";
 import SearchBar from '@/components/SearchBar.vue';
 import { useAuthStore } from '@/stores/authStore.ts';
+import { useImageStore } from '@/stores/imagesStore.ts';
 import { useUserStore } from '@/stores/userStore.ts';
 
+const authStore = useAuthStore();
+const imageStore = useImageStore();
 const userStore = useUserStore();
 </script>
 
 <template>
   <main>
       <SearchBar class="flex--just-self-end"></SearchBar>
+      <ProfilePictureWithName
+          :picture-path="imageStore.getUserImage(authStore.currentUser.imageId)"
+          :picture-name="authStore.currentUser.firstName"
+      />
       <ItemList :items="userStore.getUserList()" v-slot="{item}">
           <GeneralCard
               card-type="client"
               :profile="false"
-              :name="item.imageId"
-              :filename="'Billede af ' + item.firstName + ' ' + item.lastName"
+              :name="'Billede af ' + item.firstName + ' ' + item.lastName"
+              :filename="imageStore.getUserImage(item.imageId)"
           >
               <p>{{item.caseId[0]}}</p>
               <p>{{item.firstName}}</p>
