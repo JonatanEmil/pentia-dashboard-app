82c1fd3dd57c9bd3433127b10a08e654bb7bc6b9
nannaglaw1 <nannathedanishgirl@gmail.com>
2026-05-05 10:05:08 +0200

    Add client profile view #52

--- src/views/client/ProfileView.vue
+++ src/views/client/ProfileView.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
+import GeneralCard from '@/components/GeneralCard.vue';
+import TitleWithText from '@/components/TitleWithText.vue';
+import { type AuthUser, useAuthStore } from '@/stores/authStore';
+import { type User, useUserStore } from '@/stores/userStore';
+import { useImageStore } from '@/stores/imagesStore';
+import { ref, onMounted } from 'vue';
+
+const authStore = useAuthStore();
+const userStore = useUserStore();
+
+const user = ref<User>();
+const authUser = ref<AuthUser | null>(null);
+
+onMounted(async () => {
+    user.value = await userStore.getUser(authStore.currentUser?.id  ?? '');
+    authUser.value = authStore.currentUser;
+});
+
+
+
+
 
 </script>
 
 <template>
-    <div>
+    <main>
+        
+        <GeneralCard cardType="client" :profile="true" :card-background="false" 
+        filename="src/assets/img/users/kenny.png" alt="Kenny" class="globalmargins" >
+        <p>{{ user?.firstName }} {{ user?.lastName }}</p>
+                <p>{{ authUser?.email }}</p>
+                <p>{{ user?.phoneNumber }}</p>
+        </GeneralCard>
+            
 
-    </div>
+    </main>
 </template>
 
 <style scoped>
