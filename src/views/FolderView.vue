b3bfe962ee6041b73012054db0ab8494872dfaef
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2026-05-06 12:17:24 +0200

    add FolderView.vue Template #89

--- src/views/FolderView.vue
+++ src/views/FolderView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-
+import ReturnButton from '@/components/common/ReturnButton.vue';
+import ItemList from '@/components/ItemList.vue';
+import CarouselHandler from '@/components/CarouselHandler.vue';
 </script>
 
+
 <template>
-    <div>
+    <ReturnButton class="mt--4 mx--4"/>
+    <ItemList title="Mappen" items="">
 
-    </div>
+    </ItemList>
 </template>
-
-<style scoped>
-
-</style>
