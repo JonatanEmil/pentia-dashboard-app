7ef882e899dde75a4a72cfe16db1e5b3ad7046ba
JonatanEmil <158561754+JonatanEmil@users.noreply.github.com>
2026-05-07 12:20:13 +0200

    Delete src/components/addImageButton.vue

--- src/components/addImageButton.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import PlusIcon from '@/assets/icons/PlusIcon.vue';
-</script>
-
-<template>
-    <button class="addImageButton">
-        <PlusIcon class="m--4"></PlusIcon>
-    </button>
-</template>
