04e315cc8bdd1d244f1fe02b906dec32a9af96e9
Valdemar <pizza27@live.dk>
2026-05-08 09:33:39 +0200

    test areas removed from view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 import NavbarContainer from '@/components/NavbarContainer.vue';
 
 </script>
@@ -7,14 +7,6 @@ import NavbarContainer from '@/components/NavbarContainer.vue';
 <template>
   <header class="h--100">
     <NavbarContainer />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/Nannatestarea">Nanna Test Area</RouterLink>
-        <RouterLink to="/Jonatantestarea">Jonatan Test Area</RouterLink>
-        <RouterLink to="/valdemartestarea"> Valdemar Test Area </RouterLink>
-      </nav>
-    </div>
   </header>
 
   <RouterView class="mainContent p--0 m--0" />
